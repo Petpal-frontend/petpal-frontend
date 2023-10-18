bfeff9a11d1e0f7076aa06a65498083325b26494
jiwanchoi <wldhks303@naver.com>
2023-10-18 20:44:22 +0900

    :lipstick: ScreenLayout modify
    스플래시,globalstyle로 기본 레이아웃 가운데로 설정

--- petpal/src/styles/GlobalStyle.jsx
+++ petpal/src/styles/GlobalStyle.jsx
@@ -2,20 +2,13 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-	// ${reset};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #fffad0;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    margin: auto;
-    width: 60%;
-
+    ${reset};
+    .screenLayout{
+            height: 100vh;
+            background-color: #fffad0;
+            margin: auto;
+            width: 50%;
+},
 `;
 
 export default GlobalStyle;
