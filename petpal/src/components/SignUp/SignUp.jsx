9463a3d9b1d89e1892e546ae7badb3068bab5ebb
jiwanchoi <wldhks303@naver.com>
2023-10-24 17:32:21 +0900

    :sparkles: Feature: address
    signup 페이지에 주소 기능 추가,

--- petpal/src/components/SignUp/SignUp.jsx
+++ petpal/src/components/SignUp/SignUp.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { postSignUp } from '../../api/signUpApi';
+import { uploadImg } from '../../api/imageApi';
+import AddressSearch from '../AddressSearch/AddressSearch';
 
 const SignUpForm = () => {
   const navigate = useNavigate();
@@ -12,7 +14,46 @@ const SignUpForm = () => {
   const [image, setImage] = useState(
     'https://api.mandarin.weniv.co.kr/Ellipse.png',
   );
-  const [info, setInfo] = useState('');
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const [intro, setIntro] = useState('');
+  const [latestValue, setLatestValue] = useState(1);
+
+  //받아온 주소값 짤라서 intro에 넣어줌
+  const handleAddressSelect = address => {
+    setIntro(address.split(' ')[0]);
+  };
+
+  //accountname에 petpal_ + 현재시각(절대 안겹치게 했어요) 부여해주는 로직
+  useEffect(() => {
+    const generateUniqueAccountname = () => {
+      const currentTime = new Date();
+      const timestamp = `${currentTime.getFullYear()}${(
+        currentTime.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, '0')}${currentTime
+        .getDate()
+        .toString()
+        .padStart(2, '0')}${currentTime
+        .getHours()
+        .toString()
+        .padStart(2, '0')}${currentTime
+        .getMinutes()
+        .toString()
+        .padStart(2, '0')}${currentTime
+        .getSeconds()
+        .toString()
+        .padStart(2, '0')}`;
+      const uniqueAccountname = `petpal_${timestamp}`;
+      return uniqueAccountname;
+    };
+
+    // 회원가입 폼이 마운트될 때 자동으로 accountname 생성
+    const generatedAccountname = generateUniqueAccountname();
+    setAccountname(generatedAccountname);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // 빈 배열을 전달하여 마운트될 때 한 번만 실행되도록 함
 
   const Signup = async () => {
     try {
@@ -23,7 +64,7 @@ const SignUpForm = () => {
           password,
           accountname,
           image,
-          info,
+          intro,
         },
       };
 
@@ -75,29 +116,8 @@ const SignUpForm = () => {
           }}
         />
       </div>
-      <div className="signup_input">
-        <label>accountname</label>
-        <br />
-        <input
-          type="text"
-          placeholder="accountname"
-          onChange={e => {
-            setAccountname(e.target.value);
-          }}
-        />
-      </div>
-      <div className="signup_input">
-        <label>info</label>
-        <br />
-        <input
-          type="text"
-          placeholder="info"
-          onChange={e => {
-            setInfo(e.target.value);
-          }}
-        />
-      </div>
-      <button onClick={Signup}>singup</button>
+      <AddressSearch onAddressSelect={handleAddressSelect} />
+      <button onClick={Signup}>signup</button>
     </div>
   );
 };
