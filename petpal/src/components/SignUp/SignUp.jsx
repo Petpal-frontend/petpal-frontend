274121060f2d3146333da140cbc43d189158c9b1
jiwanchoi <wldhks303@naver.com>
2023-10-24 09:22:04 +0900

    :rocket: Feature: signup
    signup

--- petpal/src/components/SignUp/SignUp.jsx
+++ petpal/src/components/SignUp/SignUp.jsx
@@ -1,51 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SignupInput from '../Common/Input/SignupInput';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const apiUrl = 'https://api.mandarin.weniv.co.kr/user';
 
-import Input from '../Common/Input/Input';
+const SignUpForm = () => {
+  const navigate = useNavigate();
 
-import {
-  SignUpContainer,
-  H1,
-  ProfileImgBox,
-  ProfileImg,
-  ProfileUpload,
-  SearchImg,
-} from './SignUpStyle';
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [accountname, setAccountname] = useState('');
+  const [image, setImage] = useState(
+    'https://api.mandarin.weniv.co.kr/Ellipse.png',
+  );
+  const [info, setInfo] = useState('');
 
-export default function SignUp() {
-  const imgProfile = '/images/profile.svg';
-  const imgProfileBtn = '/images/profile-btn.svg';
-  const imgSearch = '/images/icon-search.svg';
+  const Signup = () => {
+    axios
+      .post(apiUrl, {
+        username: username,
+        email: email,
+        password: password,
+        accountname: accountname,
+        image: image,
+        info: info,
+      })
+      .then(response => {
+        console.log(response);
+        alert('회원가입성공');
+        if (response.status === 200) {
+          return navigate('/login');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
-    <SignUpContainer>
-      <H1>이메일로 회원가입</H1>
-      <ProfileImgBox>
-        <ProfileImg src={imgProfile} alt="프로필 기본 이미지" />
-        <ProfileUpload src={imgProfileBtn} alt="사진 업로드 버튼 이미지" />
-      </ProfileImgBox>
-      <form>
-        <Input
+    <div className="signup">
+      <div className="signup_input">
+        <label>name</label>
+        <br />
+        <input
           type="text"
-          label="닉네임"
-          placeholder="닉네임을 입력해 주세요."
+          placeholder="username"
+          onChange={e => {
+            setUsername(e.target.value);
+          }}
         />
-        <Input
+      </div>
+      <div className="signup_input">
+        <label>Email</label>
+        <br />
+        <input
           type="text"
-          label="이메일"
-          placeholder="이메일을 입력해 주세요."
+          placeholder="email"
+          onChange={e => {
+            setEmail(e.target.value);
+          }}
         />
-        <Input
+      </div>
+      <div className="signup_input">
+        <label>password</label>
+        <br />
+        <input
           type="password"
-          label="비밀번호"
-          placeholder="비밀번호를 입력해 주세요."
+          placeholder="password"
+          onChange={e => {
+            setPassword(e.target.value);
+          }}
         />
-        <Input
-          type="text"
-          label="주소"
-          placeholder="예) 문래동 강서타워, 테헤란로"
+      </div>
+      <div className="signup_input">
+        <label>accountname</label>
+        <br />
+        <input
+          type="accountname"
+          placeholder="accountname"
+          onChange={e => {
+            setAccountname(e.target.value);
+          }}
         />
-        <SearchImg src={imgSearch} alt="검색 돋보기 아이콘" />
-      </form>
-    </SignUpContainer>
+      </div>
+      <div className="signup_input">
+        <label>info</label>
+        <br />
+        <input
+          type="info"
+          placeholder="info"
+          onChange={e => {
+            setInfo(e.target.value);
+          }}
+        />
+      </div>
+      <button onClick={Signup}>singup</button>
+    </div>
   );
-}
+};
+
+export default SignUpForm;
