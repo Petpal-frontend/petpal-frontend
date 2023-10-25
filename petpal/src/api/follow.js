d1fb4b48ebc408473644c454c9e04aa0dd5ccb7f
jiwanchoi <wldhks303@naver.com>
2023-10-25 17:14:56 +0900

    :sparkles: Feat: follower
    follwer 페이지 구현(버튼 진행중)

---
+++ petpal/src/api/follow.js
@@ -0,0 +1,31 @@
+import { tokenInstance } from './axiosInstance';
+
+/* 팔로워 리스트 */
+export const getFollowers = async accountname => {
+  const response = await tokenInstance.get(
+    `/profile/${accountname}/follow/?limit=Number&skip=Number`,
+  );
+  return response;
+};
+
+/* 팔로잉 리스트 */
+export const getFollowingList = async accountname => {
+  const response = await tokenInstance.get(
+    `/profile/${accountname}/following/?limit=Number&skip=Number`,
+  );
+  return response.data;
+};
+
+/* 팔로우 */
+export const postFollow = async accountname => {
+  const response = await tokenInstance.post(`/profile/${accountname}/follow`);
+  return response.data;
+};
+
+/* 언팔로우 */
+export const deleteFollow = async accountname => {
+  const response = await tokenInstance.delete(
+    `/profile/${accountname}/unfollow`,
+  );
+  return response.data;
+};
--- petpal/src/pages/FollowPage/FollowerPage.jsx
+++ petpal/src/pages/FollowPage/FollowerPage.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import Header from '../../components/Common/Header/Header';
+import FollowerList from '../../components/Follow/FollowerList';
 
+const followerList = [
+  {
+    imgSrc: 'images/exDogImg.jpg',
+    user: '식빵맘',
+    follow: '',
+  },
+  {
+    imgSrc: 'images/exDogImg.jpg',
+    user: '식빵맘',
+    follow: '',
+  },
+  {
+    imgSrc: 'images/exDogImg.jpg',
+    user: '식빵맘',
+    follow: '',
+  },
+  {
+    imgSrc: 'images/exDogImg.jpg',
+    user: '식빵맘',
+    follow: '',
+  },
+];
 export default function FollowerPage() {
-  return <div>FollowerPage</div>;
+  return (
+    <>
+      <Header type="walk" title="Followers" />
+      <FollowerList followerList={followerList} />
+    </>
+  );
 }
