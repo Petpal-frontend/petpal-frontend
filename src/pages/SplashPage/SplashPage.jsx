158f22fbee221898eef147e25bd87d0c5ccaafc0
jiwanchoi <wldhks303@naver.com>
2023-12-03 22:16:23 +0900

    :bug: Fix/loginRefresh
    Fix/loginRefresh

--- src/pages/SplashPage/SplashPage.jsx
+++ src/pages/SplashPage/SplashPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import HomePage from '../HomePage/HomePage';
 import Splash from '../../components/Splash/Splash';
 import NavBar from '../../components/Common/NavBar/NavBar';
-
+import LoginPage from '../LoginPage/LoginPage';
 export default function SplashPage() {
   const [showSplash, setShowSplash] = useState(true);
 
@@ -12,15 +12,13 @@ export default function SplashPage() {
       setShowSplash(false);
     }, 1500); // 1.5초 후 스플래시 화면을 사라지게 함
   }, []);
-
   return (
     <div>
       {showSplash ? (
         <Splash />
       ) : (
         <>
-          <HomePage />
-          <NavBar />
+          <LoginPage />
         </>
       )}
     </div>
